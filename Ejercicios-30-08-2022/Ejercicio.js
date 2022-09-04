f3bb5a552c49be56336f6716276b727e69f19f79
anaagudel <valeagudelob10@gmail.com>
2022-09-04 17:00:15 -0500

    Ejercicios-30-08-2022

---
+++ Ejercicios-30-08-2022/Ejercicio.js
@@ -0,0 +1,53 @@
+class Paciente{
+    constructor(nombre,edad){
+        this._nombre=nombre;
+        this._edad=edad
+    }
+    consultarDatos(){
+
+    }
+}
+
+class Clinica{
+    constructor(numero,fecha){
+        this._numero=numero;
+        this._fecha=fecha;
+    }
+    cambiarFecha(){
+
+    }
+}
+
+class Medico{
+    constructor(numero,especialidad){
+        this._numero=numero;
+        this._especialidad=especialidad;
+    }
+}
+
+class Tratamiento{
+    constructor(numero,especialidad){
+        this._numero=numero;
+        this._especialidad=especialidad;
+    }
+    consultarPerfil(){
+
+    }
+}
+
+class Tratamiento{
+    constructor(descripcion){
+        this._descripcion=descripcion
+    }
+    verTratamiento(){
+
+    }
+}
+
+class Cirugia{
+
+}
+
+class Medicamento{
+
+}
